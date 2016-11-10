b2e61afc69edcaad8cef2947f1fafbb87becb9f4
Hong Quach <hong.t.quach@gmail.com>
2016-11-09 21:01:45 -0800

    Added loop to query the data every one seconds and insert into database for 24 hours.

--- src/com/htquach/Sample_json_jdbc/Main.java
+++ src/com/htquach/Sample_json_jdbc/Main.java
@@ -1,8 +1,16 @@
 /**
- * Copyright (c) 2015 Hong Quach
- *
+ * Copyright (c) 2016 Hong Quach
+ * <p>
  * This source file is licensed under the "MIT License."  Please see the LICENSE
  * in this distribution for license terms.
+ * This program includes functions to interact with live data feed and PostgreSQL
+ * 1) A method to execute query GTFS data against PostgreSQL;
+ * 2) A method to retrieve TriMet realtime data through TriMet Webservices
+ * 3) A method to insert data into PostgreSQL
+ * This program includes functions to interact with live data feed and PostgreSQL
+ * 1) A method to execute query GTFS data against PostgreSQL;
+ * 2) A method to retrieve TriMet realtime data through TriMet Webservices
+ * 3) A method to insert data into PostgreSQL
  */
 
 /** This program includes functions to interact with live data feed and PostgreSQL
@@ -27,85 +35,19 @@ import java.util.Iterator;
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-        //AccessPostgreSQL();
-        //GetTrimetVehiclesPosition();
-
-        InsertVehiclesFeedToSQL();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException, InterruptedException {
+        // AccessPostgreSQL();
+        // GetTrimetVehiclesPosition();
+        int seconds_per_day = 60 * 60 * 24;
+        for (int i = 0; i < seconds_per_day; i++) {
+            Thread.sleep(1000);
+            System.out.println(String.format("%.3f%%", (i / ((double) seconds_per_day) * 100)));
+            System.out.println("Insert " + i + " of " + seconds_per_day);
+            InsertVehiclesFeedToSQL();
+        }
     }
 
-    private static void InsertVehiclesFeedToSQL() throws ClassNotFoundException, SQLException, IOException {
-        Connection conn = GetDBConn();
-        Statement stmt = conn.createStatement();
-        ResultSet colTypesResult = stmt.executeQuery("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'vehicles_log';");
-
-        HashMap<String, String> colTypes = new HashMap<>();
-        while (colTypesResult.next()) {
-            colTypes.put(colTypesResult.getString(1), colTypesResult.getString(2));
-        }
-
-        JSONObject jsonObject = GetVehiclesFeed();
-        Long queryTime = jsonObject.getJSONObject("resultSet").getLong("queryTime");
-        JSONArray vehicles = jsonObject.getJSONObject("resultSet").getJSONArray("vehicle");
-
-        Iterator<String> keysIter = vehicles.getJSONObject(0).keys();
-        ArrayList<String> keys = new ArrayList<>();
-        while (keysIter.hasNext()) {
-            keys.add(keysIter.next());
-        }
-
-        StringBuilder insertStmt = new StringBuilder();
-        insertStmt.append("INSERT INTO \"GTFS\".vehicles_log\n");
-        insertStmt.append("\t(");
-        insertStmt.append("\"queryTime\", ");
-        int keyCount = keys.size();
-        for (int i = 0; i < keyCount; i++) {
-            insertStmt.append("\"" + keys.get(i) + "\"");
-            if (i < keyCount - 1) {
-                insertStmt.append(", ");
-            }
-        }
-        insertStmt.append(")\n");
-        insertStmt.append("VALUES\n");
-
-        int vehicleCount = vehicles.length();
-        for (int m = 0; m < vehicleCount; m++) {
-            JSONObject vehicle = vehicles.getJSONObject(m);
-            insertStmt.append("\t(");
-            insertStmt.append(queryTime + ", ");
-            for (int n = 0; n < keyCount; n++) {
-                String key = keys.get(n);
-                switch (colTypes.get(key)) {
-                    case "integer":
-                        insertStmt.append(vehicle.optInt(key));
-                        break;
-                    case "boolean":
-                        insertStmt.append(vehicle.optBoolean(key));
-                        break;
-                    case "real":
-                        insertStmt.append(vehicle.optDouble(key));
-                        break;
-                    case "bigint":
-                        insertStmt.append(vehicle.optBigInteger(key, null));
-                        break;
-                    default:
-                        insertStmt.append("'" + vehicle.optString(key) + "'");
-                }
-                if (n < keyCount - 1) {
-                    insertStmt.append(", ");
-                }
-            }
-
-            insertStmt.append(")");
-            if (m < vehicleCount - 1) {
-                insertStmt.append(",\n");
-            }
-        }
-        insertStmt.append(";");
-        stmt.execute(insertStmt.toString());
-    }
-
-    private static void AccessPostgreSQL() throws ClassNotFoundException, SQLException {
+    public static void AccessPostgreSQL() throws ClassNotFoundException, SQLException {
         Connection conn = GetDBConn();
         Statement stmt = conn.createStatement();
         ResultSet resultSet = stmt.executeQuery("SELECT DISTINCT stop_name FROM \"GTFS\".stops;");
@@ -115,6 +57,105 @@ public class Main {
         }
         resultSet.close();
         stmt.close();
+    }
+
+    public static void GetTrimetVehiclesPosition() throws IOException {
+        JSONObject jsonObject = GetVehiclesFeed();
+        JSONArray vehicles = jsonObject.getJSONObject("resultSet").getJSONArray("vehicle");
+        for (int i = 0; i < vehicles.length(); i++) {
+            System.out.println(i + "   \t>>> " + vehicles.getJSONObject(i).optString("signMessage"));
+        }
+    }
+
+    public static void InsertVehiclesFeedToSQL() throws ClassNotFoundException, IOException, InterruptedException {
+
+        int seconds_per_day = 60 * 60 * 24;
+
+        Connection conn = null;
+        try {
+            conn = GetDBConn();
+            Statement stmt = conn.createStatement();
+            ResultSet colTypesResult = stmt.executeQuery("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'vehicles_log';");
+
+            HashMap<String, String> colTypes = new HashMap<>();
+            while (colTypesResult.next()) {
+                colTypes.put(colTypesResult.getString(1), colTypesResult.getString(2));
+            }
+
+            for (int w = 0; w < seconds_per_day; w++) {
+                Thread.sleep(1000);
+                System.out.println(String.format("%.3f%%", (w / ((double) seconds_per_day) * 100)));
+                System.out.println("Insert " + w + " of " + seconds_per_day);
+
+                JSONObject jsonObject = GetVehiclesFeed();
+                Long queryTime = jsonObject.getJSONObject("resultSet").getLong("queryTime");
+                JSONArray vehicles = jsonObject.getJSONObject("resultSet").getJSONArray("vehicle");
+
+                Iterator<String> keysIter = vehicles.getJSONObject(0).keys();
+                ArrayList<String> keys = new ArrayList<>();
+                while (keysIter.hasNext()) {
+                    keys.add(keysIter.next());
+                }
+
+                StringBuilder insertStmt = new StringBuilder();
+                insertStmt.append("INSERT INTO \"GTFS\".vehicles_log\n");
+                insertStmt.append("\t(");
+                insertStmt.append("\"queryTime\", ");
+                int keyCount = keys.size();
+                for (int i = 0; i < keyCount; i++) {
+                    insertStmt.append("\"" + keys.get(i) + "\"");
+                    if (i < keyCount - 1) {
+                        insertStmt.append(", ");
+                    }
+                }
+                insertStmt.append(")\n");
+                insertStmt.append("VALUES\n");
+
+                int vehicleCount = vehicles.length();
+                for (int m = 0; m < vehicleCount; m++) {
+                    JSONObject vehicle = vehicles.getJSONObject(m);
+                    insertStmt.append("\t(");
+                    insertStmt.append(queryTime + ", ");
+                    for (int n = 0; n < keyCount; n++) {
+                        String key = keys.get(n);
+                        switch (colTypes.get(key)) {
+                            case "integer":
+                                insertStmt.append(vehicle.optInt(key));
+                                break;
+                            case "boolean":
+                                insertStmt.append(vehicle.optBoolean(key));
+                                break;
+                            case "real":
+                                insertStmt.append(vehicle.optDouble(key));
+                                break;
+                            case "bigint":
+                                insertStmt.append(vehicle.optBigInteger(key, null));
+                                break;
+                            default:
+                                insertStmt.append("'" + vehicle.optString(key) + "'");
+                        }
+                        if (n < keyCount - 1) {
+                            insertStmt.append(", ");
+                        }
+                    }
+
+                    insertStmt.append(")");
+                    if (m < vehicleCount - 1) {
+                        insertStmt.append(",\n");
+                    }
+                }
+                insertStmt.append(";");
+                stmt.execute(insertStmt.toString());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                //Ignore Exceptio
+            }
+        }
     }
 
     private static Connection GetDBConn() throws ClassNotFoundException, SQLException {
@@ -139,19 +180,11 @@ public class Main {
             db_schema = "public";
         }
 
-        String dbURL = "jdbc:postgresql://"+db_host+"/"+db_name+"?user="+db_user+"&password="+db_pw+"&currentSchema="+db_schema;
+        String dbURL = "jdbc:postgresql://" + db_host + "/" + db_name + "?user=" + db_user + "&password=" + db_pw + "&currentSchema=" + db_schema;
         Connection conn = null;
         Class.forName("org.postgresql.Driver");
         conn = DriverManager.getConnection(dbURL);
         return conn;
-    }
-
-    private static void GetTrimetVehiclesPosition() throws IOException {
-        JSONObject jsonObject = GetVehiclesFeed();
-        JSONArray vehicles = jsonObject.getJSONObject("resultSet").getJSONArray("vehicle");
-        for (int i = 0; i < vehicles.length(); i++) {
-            System.out.println(i+ "   \t>>> " + vehicles.getJSONObject(i).optString("signMessage"));
-        }
     }
 
     private static JSONObject GetVehiclesFeed() throws IOException {
@@ -160,7 +193,7 @@ public class Main {
             throw new RuntimeException("TriMet AppID is required to query realtime data.  " +
                     "Specify its value in the environment variable 'TRIMETAPPID");
         }
-        URL serviceURL = new URL("http://developer.trimet.org/ws/v2/vehicles/AppID/"+appID);
+        URL serviceURL = new URL("http://developer.trimet.org/ws/v2/vehicles/AppID/" + appID);
         InputStream inputStream = serviceURL.openStream();
         JSONTokener jsonTokener = new JSONTokener(inputStream);
         return new JSONObject(jsonTokener);
