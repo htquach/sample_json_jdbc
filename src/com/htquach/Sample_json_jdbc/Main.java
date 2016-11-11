893eb98912eb15a4a5a595008155433b1f8e9934
Hong Quach <hong.t.quach@gmail.com>
2016-11-11 15:29:04 -0800

    Pull both TTIP and ODOT vehicles position data and put into DB every minute

--- src/com/htquach/Sample_json_jdbc/Main.java
+++ src/com/htquach/Sample_json_jdbc/Main.java
@@ -3,20 +3,8 @@
  * <p>
  * This source file is licensed under the "MIT License."  Please see the LICENSE
  * in this distribution for license terms.
+ *
  * This program includes functions to interact with live data feed and PostgreSQL
- * 1) A method to execute query GTFS data against PostgreSQL;
- * 2) A method to retrieve TriMet realtime data through TriMet Webservices
- * 3) A method to insert data into PostgreSQL
- * This program includes functions to interact with live data feed and PostgreSQL
- * 1) A method to execute query GTFS data against PostgreSQL;
- * 2) A method to retrieve TriMet realtime data through TriMet Webservices
- * 3) A method to insert data into PostgreSQL
- */
-
-/** This program includes functions to interact with live data feed and PostgreSQL
- *   1) A method to execute query GTFS data against PostgreSQL;
- *   2) A method to retrieve TriMet realtime data through TriMet Webservices
- *   3) A method to insert data into PostgreSQL
  */
 
 package com.htquach.Sample_json_jdbc;
@@ -56,21 +44,26 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException, InterruptedException {
         // AccessPostgreSQL();
         // GetTrimetVehiclesPosition();
-        //InsertVehiclesFeedToSQL();
 
-        int seconds_per_day = 60 * 60 * 24;
+        int loopCount = 60 * 24;
+        boolean twoMinutesReady = false;
 
         Connection conn = null;
         try {
             conn = GetDBConn();
-            for (int w = 0; w < seconds_per_day; w++) {
-                GetTTIP(conn);
-                // Frequency of 2 minute enforced.
-                Thread.sleep(180000);
+            for (int w = 1; w <= loopCount; w++) {
+                 twoMinutesReady = !twoMinutesReady;
+                if (twoMinutesReady) {
+                    // Frequency of 2 minute enforced by TTIP data source
+                    GetTTIP(conn);
+                }
+                InsertVehiclesFeedToSQL(conn);
+                System.out.println(String.format("%.3f%%", (w / ((double) loopCount) * 100)));
+                System.out.println("Insert " + w + " of " + loopCount);
+                // Frequency of 1 minute
+                Thread.sleep(60000);
             }
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
+        } catch (ParserConfigurationException | SAXException e) {
             e.printStackTrace();
         } finally {
             if (conn != null) {
@@ -118,8 +111,6 @@ public class Main {
 
         if (doc.getDocumentElement().getTextContent().contains("too soon to retrieve data")) {
             System.out.println(doc.getDocumentElement().getTextContent());
-            System.out.println("Going to sleep for 2 minutes...");
-            Thread.sleep(120000);
             return;
         }
 
@@ -131,8 +122,6 @@ public class Main {
                 "\tVALUES\n");
         for (int i = 0; i < entries.getLength(); i++) {
             Element current = (Element) entries.item(i);
-
-
 
             stmtBuilder.append("\t(");
             stmtBuilder.append(current.getElementsByTagName("TraversalID").item(0).getTextContent());
@@ -162,6 +151,7 @@ public class Main {
             Statement stmt = conn.createStatement();
             stmt.execute(stmtBuilder.toString());
         }
+        System.out.println(stmtBuilder.toString());
     }
 
     public static void GetTrimetVehiclesPosition() throws IOException {
@@ -172,13 +162,8 @@ public class Main {
         }
     }
 
-    public static void InsertVehiclesFeedToSQL() throws ClassNotFoundException, IOException, InterruptedException {
-
-        int seconds_per_day = 60 * 60 * 24;
-
-        Connection conn = null;
+    public static void InsertVehiclesFeedToSQL(Connection conn) throws ClassNotFoundException, IOException, InterruptedException {
         try {
-            conn = GetDBConn();
             Statement stmt = conn.createStatement();
             ResultSet colTypesResult = stmt.executeQuery("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'vehicles_log';");
 
@@ -187,86 +172,76 @@ public class Main {
                 colTypes.put(colTypesResult.getString(1), colTypesResult.getString(2));
             }
 
-            for (int w = 0; w < seconds_per_day; w++) {
-                Thread.sleep(1000);
-                System.out.println(String.format("%.3f%%", (w / ((double) seconds_per_day) * 100)));
-                System.out.println("Insert " + w + " of " + seconds_per_day);
+            JSONObject jsonObject = GetVehiclesFeed();
+            Long queryTime = jsonObject.getJSONObject("resultSet").getLong("queryTime");
+            JSONArray vehicles = jsonObject.getJSONObject("resultSet").getJSONArray("vehicle");
 
-                JSONObject jsonObject = GetVehiclesFeed();
-                Long queryTime = jsonObject.getJSONObject("resultSet").getLong("queryTime");
-                JSONArray vehicles = jsonObject.getJSONObject("resultSet").getJSONArray("vehicle");
+            Iterator<String> keysIter = vehicles.getJSONObject(0).keys();
+            ArrayList<String> keys = new ArrayList<String>();
+            while (keysIter.hasNext()) {
+                keys.add(keysIter.next());
+            }
 
-                Iterator<String> keysIter = vehicles.getJSONObject(0).keys();
-                ArrayList<String> keys = new ArrayList<String>();
-                while (keysIter.hasNext()) {
-                    keys.add(keysIter.next());
+            StringBuilder insertStmt = new StringBuilder();
+            insertStmt.append("INSERT INTO \"GTFS\".vehicles_log\n");
+            insertStmt.append("\t(");
+            insertStmt.append("\"queryTime\", ");
+            int keyCount = keys.size();
+            for (int i = 0; i < keyCount; i++) {
+                insertStmt.append("\"");
+                insertStmt.append(keys.get(i));
+                insertStmt.append("\"");
+                if (i < keyCount - 1) {
+                    insertStmt.append(", ");
                 }
+            }
+            insertStmt.append(")\n");
+            insertStmt.append("VALUES\n");
 
-                StringBuilder insertStmt = new StringBuilder();
-                insertStmt.append("INSERT INTO \"GTFS\".vehicles_log\n");
+            int vehicleCount = vehicles.length();
+            for (int m = 0; m < vehicleCount; m++) {
+                JSONObject vehicle = vehicles.getJSONObject(m);
                 insertStmt.append("\t(");
-                insertStmt.append("\"queryTime\", ");
-                int keyCount = keys.size();
-                for (int i = 0; i < keyCount; i++) {
-                    insertStmt.append("\"");
-                    insertStmt.append(keys.get(i));
-                    insertStmt.append("\"");
-                    if (i < keyCount - 1) {
+                insertStmt.append(queryTime);
+                insertStmt.append(", ");
+                for (int n = 0; n < keyCount; n++) {
+                    String key = keys.get(n);
+                    String s = colTypes.get(key);
+                    if (s.equals("integer")) {
+                        insertStmt.append(vehicle.optInt(key));
+
+                    } else if (s.equals("boolean")) {
+                        insertStmt.append(vehicle.optBoolean(key));
+
+                    } else if (s.equals("real")) {
+                        insertStmt.append(vehicle.optDouble(key));
+
+                    } else if (s.equals("bigint")) {
+                        insertStmt.append(vehicle.optBigInteger(key, null));
+
+                    } else {
+                        insertStmt.append("'");
+                        insertStmt.append(vehicle.optString(key));
+                        insertStmt.append("'");
+                    }
+                    if (n < keyCount - 1) {
                         insertStmt.append(", ");
                     }
                 }
-                insertStmt.append(")\n");
-                insertStmt.append("VALUES\n");
 
-                int vehicleCount = vehicles.length();
-                for (int m = 0; m < vehicleCount; m++) {
-                    JSONObject vehicle = vehicles.getJSONObject(m);
-                    insertStmt.append("\t(");
-                    insertStmt.append(queryTime);
-                    insertStmt.append(", ");
-                    for (int n = 0; n < keyCount; n++) {
-                        String key = keys.get(n);
-                        String s = colTypes.get(key);
-                        if (s.equals("integer")) {
-                            insertStmt.append(vehicle.optInt(key));
-
-                        } else if (s.equals("boolean")) {
-                            insertStmt.append(vehicle.optBoolean(key));
-
-                        } else if (s.equals("real")) {
-                            insertStmt.append(vehicle.optDouble(key));
-
-                        } else if (s.equals("bigint")) {
-                            insertStmt.append(vehicle.optBigInteger(key, null));
-
-                        } else {
-                            insertStmt.append("'");
-                            insertStmt.append(vehicle.optString(key));
-                            insertStmt.append("'");
-                        }
-                        if (n < keyCount - 1) {
-                            insertStmt.append(", ");
-                        }
-                    }
-
-                    insertStmt.append(")");
-                    if (m < vehicleCount - 1) {
-                        insertStmt.append(",\n");
-                    }
+                insertStmt.append(")");
+                if (m < vehicleCount - 1) {
+                    insertStmt.append(",\n");
                 }
-                insertStmt.append(";");
+            }
+            insertStmt.append(";");
+
+            if (vehicleCount > 0) {
                 stmt.execute(insertStmt.toString());
+                System.out.println(insertStmt.toString());
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                //Ignore Exception
-            }
         }
     }
 
